25103571162d064145e3ac30daaa795b7d0a819a
Jonathan Cosme <i.jonathan.cosme@gmail.com>
2021-01-18 09:13:12 -0600

    fixed some bugs

--- cancerApp.py
+++ cancerApp.py
@@ -10,6 +10,7 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 from tensorflow.keras.models import load_model
+import os
 
 st.markdown("""
             # Skin Growth Classifier*  
@@ -19,20 +20,21 @@ st.markdown("""
 st.markdown("""
             This app will classify a user-uploaded photo (presumably of a skin growth) as either 'Benign' or 'Malignant.'  
               ### Steps:  
-            1. Upload a photo  
-            2. Select the 'Malignant or Benign?' button  
+            1. Take a photo of a skin growth (make sure it's VERY close-up).  
+            2. Upload the photo  
+            3. Select the 'Malignant or Benign?' button  
             """
             )
 
 uploaded_file = st.file_uploader("Step 1: Select a photo to upload.")
 
 if st.button('Malignant or Benign?'):
-    img = Image.open(uploaded_file.name)
-    img.thumbnail((224, 224), Image.ANTIALIAS)
+    img = Image.open(uploaded_file)
+    img = img.resize((224,224))
     img = np.array(img)
     st.image(img, channels="RGB")
     model = load_model('cancer.keras')
-    prob = model(img.reshape(1, 224, 224, 3), training=False)[0]
+    prob = model(np.array([img]), training=False)[0]
     if prob <= 0.5:
         prediction = 'Benign'
     else:
