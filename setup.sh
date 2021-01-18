fa405879ddb72e1ca87101bddf3c599565d3e872
Jonathan Cosme <i.jonathan.cosme@gmail.com>
2021-01-18 07:12:59 -0600

    updating setup.sh with my email

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
-mkdir -p ~/.streamlitecho "[server]
+mkdir -p ~/.streamlit/
 
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[general]\n\
+email = \"i.jonathan.cosme@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
